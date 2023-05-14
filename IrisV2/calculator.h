010fad7444216aa855bf0935a6299c84d53ff8d6
Jihed Bouazizi <64079635+Jihedbz@users.noreply.github.com>
2023-05-15 00:20:50 +0200

    Add files via upload

---
+++ IrisV2/calculator.h
@@ -0,0 +1,29 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QDialog>
+
+namespace Ui {
+class calculator;
+}
+
+class calculator : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit calculator(QWidget *parent = nullptr);
+    ~calculator();
+
+private slots:
+
+private:
+    Ui::calculator *ui;
+private slots :
+    void NumPressed();
+    void MathButtonPressed();
+    void EqualButtonPressed();
+    void ChangeNumberSign();
+};
+
+#endif // CALCULATOR_H
